4a43f1c72d09493bef80df3e19ef74abdd2009e2
Your Name <you@example.com>
2016-12-23 05:28:10 +0900

    move validation to Dim2

--- src/main/scala/com/github/agetakoyaki29/geometry/dim2/Dim2.scala
+++ src/main/scala/com/github/agetakoyaki29/geometry/dim2/Dim2.scala
@@ -7,7 +7,7 @@ object Dim2 extends Dim2Factory[Dim2] {
   def apply(d: Double): Dim2 = new SimpleD2(d, d)
   def apply(x: Double, y: Double): Dim2 = new SimpleD2(x, y)
   def apply(op: Dim2): Dim2 = new SimpleD2(op.x, op.y)
- 
+
   val ZERO = this(0, 0)
   val E1 = this(1, 0)
   val E2 = this(0, 1)
@@ -18,19 +18,35 @@ trait Dim2 extends IndexedSeq[Double] {
   def x: Double
   def y: Double
   
-  // -- IndexedSeq --
+  // -- validation --
   
+  validateDouble(x)
+  validateDouble(y)
+  
+  def validateDouble(d: Double): Unit = d match {
+    case Double.NaN =>
+      new IllegalArgumentException("Not NaN")
+    case Double.PositiveInfinity | Double.NegativeInfinity => 
+      new IllegalArgumentException("Not Infinity")
+    case Double.MinValue =>
+      new IllegalArgumentException("Not MinValue")
+    case _ =>
+  }
+
+
+  // -- IndexedSeq --
+
   override def foreach[U](f: Double => U): Unit = { f(x); f(y) }
   override def apply(idx: Int): Double = idx match { case 0 => x; case 1 => y }
   override def length: Int = 2
 
   // ----
-  
+
   def factory: Dim2Factory[_ <: Dim2] = Dim2
-  
+
   def isZero: Boolean = x==0 && y==0
   def isZeroWithDelta: Boolean = Delta.eq0(x) && Delta.eq0(y)
-  
+
   def mapD2(f: Double => Double): Dim2 = factory(f(x), f(y))
 
   // -- std --
@@ -45,23 +61,9 @@ trait Dim2 extends IndexedSeq[Double] {
     case dim2: Dim2 => Delta.eq(x, dim2.x) && Delta.eq(y, dim2.y)
     case _ => false
   }
-  
-  override def hashCode = x.## * 31 + y.##  // TODO Provisional  
+
+  override def hashCode = x.## * 31 + y.##  // TODO Provisional
 }
 
 
-final class SimpleD2(override val x: Double, override val y: Double) extends Dim2 {
-  validateDouble(x)
-  validateDouble(y)
-  
-  def validateDouble(d: Double): Unit = d match {
-    case Double.NaN =>
-      new IllegalArgumentException("Not NaN")
-    case Double.PositiveInfinity | Double.NegativeInfinity => 
-      new IllegalArgumentException("Not Infinity")
-    case Double.MinValue =>
-      new IllegalArgumentException("Not MinValue")
-    case _ =>
-  }
-}
-
+final class SimpleD2(override val x: Double, override val y: Double) extends Dim2
