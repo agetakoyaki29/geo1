0c77cd0e34fd982e1bccc99cf55cb0c5ea67a83a
Your Name <you@example.com>
2016-12-23 17:58:16 +0900

    build.sbt

--- build.sbt
+++ build.sbt
@@ -3,5 +3,9 @@ lazy val root = (project in file("."))
   .settings(
     name := "scala geometry",
     version := "0.0",
-    scalaVersion := "2.12.0"
+    scalaVersion := "2.12.0",
+
+    libraryDependencies ++= Seq(
+      "junit" % "junit" % "4.11" % "test"
+    )
   )
--- src/test/scala/com/github/agetakoyaki29/geometry/line/Dir2Test.scala
+++ src/test/scala/com/github/agetakoyaki29/geometry/line/Dir2Test.scala
@@ -6,11 +6,11 @@ import com.github.agetakoyaki29.geometry.Delta
 
 
 class Dir2Test {
-  
+
   val dir1 = Dir2(3, 4)
   val dir2 = Dir2(7, -3)
   val pt1 = Point2(-5, 1)
-  
+
 //  @Test def test1 = nearestIsOn(dir1, pt1)
   @Test def test2 = normalizedNormIs1(dir1)
   @Test def test3 = normalIsNormal(dir1)
@@ -25,38 +25,38 @@ class Dir2Test {
     assertTrue("dir nearest is on dir", t1)
     assertTrue("dir nearest is on dir(strict)", t2)
   }
-  
+
   def normalizedNormIs1(dir: Dir2) = {
     val norm = dir.normalize.norm
     assertEquals(1, norm, Delta.delta)
   }
-  
+
   def normalIsNormal(dir: Dir2) = {
     val normal = dir.normal
     val t1 = dir.isNormal(normal)
     assertTrue("dir normal is normal by dir", t1)
   }
-  
+
   def normalIsPlus90Degree(dir: Dir2) = {
     val normal = dir.normal
     val angle = dir.angleTo(normal)
     val normalized = mod(angle, 2*Math.PI)
     assertEquals(Math.PI/2, normalized, Delta.delta)
   }
-  
+
   def mod(d1: Double, d2: Double) = (d1 % d2 + d2) % d2
-  
+
   def testCosAngle(dir1: Dir2, dir2: Dir2) = {
     val deg = dir1 angleTo dir2
     val cos = dir1 cosTo dir2
     val delta = Delta.deltaMin(cos, Math.cos(deg))
     assertEquals(cos, Math.cos(deg), delta)
   }
-  
+
   def testSinAngle(dir1: Dir2, dir2: Dir2) = {
     val deg = dir1 angleTo dir2
     val sin = dir1 sinTo dir2
     assertEquals(sin, Math.sin(deg), Delta.delta)
   }
-  
+
 }
