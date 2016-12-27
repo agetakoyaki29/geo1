b252c168abfac33f98bd930769e933fac0eea235
Your Name <you@example.com>
2016-12-28 01:42:43 +0900

    add Delta.ge to fix Size2.containWD

--- src/main/scala/com/github/agetakoyaki29/geometry/Delta.scala
+++ src/main/scala/com/github/agetakoyaki29/geometry/Delta.scala
@@ -7,6 +7,11 @@ object Delta {
   def deltaMin(ds: Double*): Double = ds.map(Math.abs(_)).min * Math.pow(0.1, 15) // 16 ha dame
 
   def eq0(d: Double): Boolean = Math.abs(d) < delta    // TODO delete
-  def eq0(d: Double, delta: Double): Boolean = Math.abs(d) < delta
+  private def eq0(d: Double, delta: Double): Boolean = Math.abs(d) < delta
   def eq(d1: Double, d2: Double): Boolean = eq0(d1-d2, deltaMin(d1, d2))
+  
+  def gt0(d: Double, delta: Double): Boolean = d > -delta
+  def ge0(d: Double, delta: Double): Boolean = d >= -delta
+  def gt(d1: Double, d2: Double): Boolean = gt0(d1-d2, deltaMin(d1, d2))
+  def ge(d1: Double, d2: Double): Boolean = ge0(d1-d2, deltaMin(d1, d2))
 }
--- src/main/scala/com/github/agetakoyaki29/geometry/line/Size2.scala
+++ src/main/scala/com/github/agetakoyaki29/geometry/line/Size2.scala
@@ -2,6 +2,7 @@ package com.github.agetakoyaki29.geometry.line
 
 import com.github.agetakoyaki29.geometry.dim2.Dim2Factory
 import com.github.agetakoyaki29.geometry.dim2.Dim2
+import com.github.agetakoyaki29.geometry.Delta
 
 
 object Size2 extends Dim2Factory[Size2] {
@@ -30,7 +31,10 @@ class Size2 protected(override protected val wrapped: Dim2) extends Point2(wrapp
   def contain(pt: Point2) = 0 <= pt.x && pt.x <= x &&
                             0 <= pt.y && pt.y <= y
   
-  def containWithDelta(pt: Point2) = 0 <= pt.x && pt.x <= x &&    // FIXME delta
-                                     0 <= pt.y && pt.y <= y
+  def containWithDelta(pt: Point2) = {
+    import Delta.ge
+    ge(0, pt.x) && ge(pt.x, x) &&
+    ge(0, pt.y) && ge(pt.y, y)
+  }
   
 }
